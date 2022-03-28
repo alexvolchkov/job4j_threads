ad78ea0c6643d99b4ceccc0762057ae5b2e965ec
alexvolchkov <volkaa@mail.ru>
2022-03-28 09:02:07 +0300

    1.1. Состояние нити. [#229175 #280709]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -11,6 +11,8 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.printf("%s: %s%n", first.getName(), first.getState());
+            System.out.printf("%s: %s%n", second.getName(), second.getState());
         }
         System.out.println("Работа завершена");
     }
