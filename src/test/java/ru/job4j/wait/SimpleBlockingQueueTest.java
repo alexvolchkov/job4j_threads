ac4b14cae9ad70a1b6dabdd7f23bd1895981157e
alexvolchkov <volkaa@mail.ru>
2022-04-04 22:32:20 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -3,6 +3,9 @@ package ru.job4j.wait;
 import static org.hamcrest.core.Is.is;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.junit.Assert.assertThat;
 
 public class SimpleBlockingQueueTest {
@@ -59,5 +62,40 @@ public class SimpleBlockingQueueTest {
             consumer.getState();
         }
         assertThat(queue.getQueue().size(), is(0));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i < 5; i++) {
+                            queue.offer(i);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.getQueue().isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 }
