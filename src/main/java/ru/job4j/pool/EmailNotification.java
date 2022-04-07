d1aa84374040f7099b8e2275486c735be6a6cbc7
alexvolchkov <volkaa@mail.ru>
2022-04-07 22:24:03 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,30 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        if (pool.isShutdown()) {
+            throw new IllegalStateException("Thread pool is stopped");
+        }
+            pool.submit(
+                    () -> send(
+                            String.format("Notification %s to email %s", user.getUserName(), user.getEmail()),
+                            String.format("Add a new event to %s", user.getUserName()),
+                            user.getEmail()
+                    )
+            );
+
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
