70ce37f89542f6737a969c4e72780a818c033d82
alexvolchkov <volkaa@mail.ru>
2022-04-10 15:49:55 +0300

    4. CompletableFuture [#361626]

--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -12,10 +12,7 @@ public class RolColSumTest {
     @Test
     public void whenSumThenMatrix3() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        long start = System.currentTimeMillis();
         RolColSum.Sums[] result = RolColSum.sum(matrix);
-        long finish = System.currentTimeMillis();
-        System.out.println(finish - start);
         assertThat(result[0].getRowSum(), is(6));
         assertThat(result[1].getRowSum(), is(15));
         assertThat(result[2].getRowSum(), is(24));
@@ -27,10 +24,7 @@ public class RolColSumTest {
     @Test
     public void whenAsyncSumThenMatrix3() throws ExecutionException, InterruptedException {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        long start = System.currentTimeMillis();
         RolColSum.Sums[] result = RolColSum.asyncSum(matrix);
-        long finish = System.currentTimeMillis();
-        System.out.println(finish - start);
         assertThat(result[0].getRowSum(), is(6));
         assertThat(result[1].getRowSum(), is(15));
         assertThat(result[2].getRowSum(), is(24));
